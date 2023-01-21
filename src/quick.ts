37eaf1007d43feb1a358082b61667320dd62ada6
Michael <pinke@live.ru>
2023-01-21 22:21:09 +0600

    qmapKeys func mapper 2nd arg is a key.

--- src/quick.ts
+++ src/quick.ts
@@ -59,7 +59,7 @@ export const qmapKeys = curry2(
     for(k in keyMap) if(k in o) {
       mapped = keyMap[k]
       ;[newKey, newValue] = isFunc(mapped)
-        ? (mapped as AnyFunc)(o[k], o)
+        ? (mapped as AnyFunc)(o[k], k, o)
         : [mapped || k, o[k]]
       o[newKey] = newValue
       if(k !== newKey) delete o[k]
