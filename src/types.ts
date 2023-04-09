b7b80906b5933c1ee4d9f0b833e6a614d645d769
Michael <pinke@live.ru>
2023-04-09 07:37:56 +0600

    typing improved & bundle.

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,10 @@
 export type Cond = (x1?: any, x2?: any, x3?: any) => boolean
-export interface AnyObject {
-  [k: string]: any
-}
+export interface AnyObject { [k: string]: any }
 export type AnyArgs = any[]
+export type Reducer<T=any> = (accum: T, cur: any, index: number) => T
+export type AnyFunc<ReturnT = any, Args extends AnyArgs = AnyArgs> = (...args: Args) => ReturnT
+export type TupleFn<ARG1=any, ARG2=any, Out=any> = (a: ARG1, b: ARG2) => Out
 export type Curried<
   Args extends AnyArgs = AnyArgs,
   ReturnT = any
 > = (arg: Args[number]) => Curried<Args> | ReturnT
-export type Reducer<T=any> = (accum: T, cur: any, index: number) => T
-export type AnyFunc<
-  ReturnT = any,
-  Args extends AnyArgs = AnyArgs
-> = (...args: Args) => ReturnT
-export type TupleFn<ARG1=any, ARG2=any, Out=any> = (a: ARG1, b: ARG2) => Out
