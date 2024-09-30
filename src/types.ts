d01f75197293c2df20044b0b58316327b49d84d2
Michael <pinke@live.ru>
2024-09-30 13:15:48 +0300

    better typing.

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 import { AnyArgs } from "./internal_types"
 
 export type Cond = (x1?: any, x2?: any, x3?: any) => boolean
-export interface AnyObject { [k: string]: any }
+export interface AnyObject extends Record<any, any> {}
 export type Reducer<T=any> = (accum: T, cur: any, index: number) => T
 export type AnyFunc<ReturnT = any, Args extends AnyArgs = AnyArgs> = (...args: Args) => ReturnT
 export type Curried<Args extends AnyArgs = AnyArgs, ReturnT = any> = (arg: Args[number]) => Curried<Args> | ReturnT
